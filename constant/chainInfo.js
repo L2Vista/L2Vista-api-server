71bdef64948f7ce691a95b1324cda578a3f9ddc6
hyunkicho <dmsrlgusrl@naver.com>
2023-08-13 06:34:42 +0900

    chore: indent

--- constant/chainInfo.js
+++ constant/chainInfo.js
@@ -1,4 +1,5 @@
-const chianNameAndChainID = [["base-testnet" , 84531],["zora-testnet" , 999],["optimism-goerli" , 420]]
+const chianNameAndChainID = 
+[["base-testnet" , 84531],["zora-testnet" , 999],["optimism-goerli" , 420]]
 
 module.exports = {
     chianNameAndChainID
